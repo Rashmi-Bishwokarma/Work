64e14b388ea69570a4cc9cde89e7e7a1481bb041
Rashmi-Bishwokarma <rashmibishwokarma15@gmail.com>
2023-12-24 13:18:51 +0545

    Added my project

--- lib/profile.dart
+++ lib/profile.dart
@@ -243,7 +243,7 @@ class Profile extends StatelessWidget {
                           width: 180,
                         ),
                         Text(
-                          'View all',
+                          'View',
                           style: TextStyle(
                             color: Color(0xFF070707),
                             fontSize: 16,
